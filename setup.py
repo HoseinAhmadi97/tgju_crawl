bb74d69c0cde5e22e64d630cdee9c8ed50a7d363
HoseinAhmadi97 <hosein.ahmadi1997@yahoo.com>
2023-03-30 21:19:16 +0330

    update

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='tgju_crawl',                           # should match the package folder
     packages=['tgju_crawl'],                     # should match the package folder
-    version='1.0.0',                                # important for updates
+    version='1.0.1',                                # important for updates
     license='BSD (3-clause)',                                  # should match your chosen license
     description='A Python Module to Access tgju.org Historical Price',
     long_description=long_description,              # loads your README.md
     long_description_content_type="text/markdown",  # README.md is of type 'markdown'
     author='HOSSEIN AHMADI',
     author_email='hosein.ahmadi1997@yahoo.com',
-    install_requires=['requests','jdatetime','pandas','bs4','calendar','datetime','lxml'],                  # list all packages that your package uses
+    install_requires=['requests','jdatetime','pandas','bs4','datetime','lxml'],                  # list all packages that your package uses
     
 )
